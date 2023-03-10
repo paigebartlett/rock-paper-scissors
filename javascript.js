190a0bc38c70859bd2e28f85f82c53806ef41a7b
paigebartlett <77411738+paigebartlett@users.noreply.github.com>
2023-03-10 12:15:14 -0800

    Create seperate getPlayerSelection function

--- javascript.js
+++ javascript.js
@@ -14,11 +14,13 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+function getPlayerSelection() {
+    let selection = (prompt("Choose rock, paper, or scissors")).toLowerCase();
+    if (selection != "rock" && selection != "paper" && selection != "scissors") {
+        selection = (prompt("That's not a valid answer! Choose rock, paper, or scissors")).toLowerCase();
+    }
+}
+
 function playRound() {
     computerChoice = getComputerChoice();
-    playerSelection = (prompt("Choose rock, paper, or scissors")).toLowerCase();
-    if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
-        playerSelection = (prompt("That's not a valid answer! Choose rock, paper, or scissors")).toLowerCase();
-    }
-
 }
