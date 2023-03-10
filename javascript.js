f9833a4873ff84356e3b6b5e20ad1bcb44dfe1ee
paigebartlett <77411738+paigebartlett@users.noreply.github.com>
2023-03-10 11:41:22 -0800

    Test getComputerChoice() functionality

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 function getComputerChoice(randomNumber(3)) {
     let randomBelowThree = Math.floor(Math.random * 3);
-    let computerChoice;
+    console.log(randomBelowThree)
+    console.log(computerChoice)
     if (randomBelowThree === 0) {
         computerChoice = "rock";
     }
@@ -10,5 +11,8 @@ function getComputerChoice(randomNumber(3)) {
     else {
         computerChoice = "scissors";
     }
+    console.log(computerChoice)
     return computerChoice;
 }
+
+getComputerChoice();
